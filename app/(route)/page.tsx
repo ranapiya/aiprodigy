e225762b7d39e93fabb9261e6d80e55cd20eb292
PRIYA RANA <127029448+ranapiya@users.noreply.github.com>
2024-08-03 21:11:05 +0530

    initial commit

---
+++ app/(route)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
